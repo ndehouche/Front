918a63e51293958522cea233712bffa90a05222b
Nassim Dehouche <65496410+ndehouche@users.noreply.github.com>
2021-11-01 08:39:18 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+function addRows(_testator, id, _reward) {
+  var table=document.getElementById('willsTable');
+      row = table.insertRow(-1);
+      
+      cell1 = row.insertCell(0),
+      cell2 = row.insertCell(1);
+      cell3 = row.insertCell(2);
 
+  cell1.innerHTML = _testator;
+  cell2.innerHTML = id;
+  cell3.innerHTML = _reward;
+}
+function DeleteRows() {
+  var table=document.getElementById('willsTable');
+        var rowCount = table.rows.length;
+        for (var i = rowCount - 1; i > 0; i--) {
+            table.deleteRow(i);
+        }
+    }
+async function checkConnected  (){
+  accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    if (accounts.length == 0){
+    console.log("User is not logged in to MetaMask");
+    document.getElementById('div-interact').style.display = "none";
+     document.getElementById('wallet-button').innerHTML="🦊 Connect Wallet";
+    this.alert("Please connect to Metamask");
+}
+}
 
 var accounts=[];
-const contractAddress='0xAb9bf7078068b17764237E858E7525745506DE0e';
+const contractAddress='0xdbFd69d1dEe192Bc4b036b64D27E356De9258df7';
 const contractABI=[
 	{
 		"anonymous": false,
@@ -147,10 +174,6 @@ const contractABI=[
 		],
 		"name": "willUpdated",
 		"type": "event"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "fallback"
 	},
 	{
 		"inputs": [],
@@ -334,12 +357,154 @@ const contractABI=[
 		"type": "function"
 	},
 	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_testator",
+				"type": "address"
+			}
+		],
+		"name": "getWills",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "value",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "reward",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "maturity",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address payable",
+						"name": "testator",
+						"type": "address"
+					},
+					{
+						"internalType": "address payable",
+						"name": "beneficiary",
+						"type": "address"
+					}
+				],
+				"internalType": "struct EtherTrust.Will[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "lastAlive",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "numTestators",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "testators",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "wills",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "reward",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "maturity",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address payable",
+				"name": "testator",
+				"type": "address"
+			},
+			{
+				"internalType": "address payable",
+				"name": "beneficiary",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
 		"stateMutability": "payable",
 		"type": "receive"
 	}
 ];
-
+//Need to handle when user disconnects all metamask accounts
 async function connectWallet(){
+  
     accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     if (accounts.length == 0){
     console.log("User is not logged in to MetaMask");
@@ -348,6 +513,7 @@ async function connectWallet(){
 }
     else{
     this.alert("Here is your current account: " + accounts[0]);
+    document.getElementById('wallet-button').innerHTML="🦊 Connected";
     document.getElementById('div-interact').style.display = "block";
     var web3=new Web3(window.ethereum);
     const EtherTrust= new web3.eth.Contract(contractABI, contractAddress);
@@ -355,6 +521,7 @@ async function connectWallet(){
     //Create a will
     const create=document.getElementById('create-button');
     create.onclick = async ()=> {
+    checkConnected();  
     _reward=document.getElementById("reward").value; 
     _maturity=document.getElementById("maturity").value; 
     _beneficiary=document.getElementById("beneficiary").value;   
@@ -363,15 +530,18 @@ async function connectWallet(){
 	   }
     const alive=document.getElementById('alive-button');
     alive.onclick = async ()=> {
+    checkConnected();  
     await EtherTrust.methods.IAmAlive().send({from: ethereum.selectedAddress, to:contractAddress});
     }
      const cancel=document.getElementById('cancel-button');
     cancel.onclick = async ()=> {
+    checkConnected();  
     _id=document.getElementById("id").value; 
     await EtherTrust.methods.cancelWill(_id).send({from: ethereum.selectedAddress, to:contractAddress});
     }
   const value=document.getElementById('value-button');
     value.onclick = async ()=> {
+    checkConnected();  
     _id=document.getElementById("idv").value; 
     _decrease=-document.getElementById("decreasev").value; 
     if (_decrease==0){ this.alert("This amount cannot be zero.");}
@@ -381,6 +551,7 @@ async function connectWallet(){
 
   const reward=document.getElementById('reward-button');
     reward.onclick = async ()=> {
+    checkConnected();  
     _id=document.getElementById("idr").value; 
     _decrease=-document.getElementById("decreaser").value; 
     if (_decrease==0){ this.alert("This amount cannot be zero.");}
@@ -390,6 +561,7 @@ async function connectWallet(){
 
   const beneficiary=document.getElementById('beneficiary-button');
     beneficiary.onclick = async ()=> {
+    checkConnected();  
     _id=document.getElementById("idb").value; 
     _newBeneficiary=document.getElementById("newbeneficiary").value; 
     await EtherTrust.methods.changeBeneficiary(_id, _newBeneficiary).send({from: ethereum.selectedAddress, to:contractAddress});
@@ -397,6 +569,7 @@ async function connectWallet(){
 
   const maturity=document.getElementById('maturity-button');
     maturity.onclick = async ()=> {
+    checkConnected();  
     _id=document.getElementById("idm").value; 
     _newMaturity=document.getElementById("newmaturity").value; 
     await EtherTrust.methods.changeMaturity(_id, _newMaturity).send({from: ethereum.selectedAddress, to:contractAddress});
@@ -404,9 +577,47 @@ async function connectWallet(){
   
   const execute=document.getElementById('execute-button');
     execute.onclick = async ()=> {
+    checkConnected();  
     _testator=document.getElementById("exectestator").value; 
     _id=document.getElementById("ide").value; 
     await EtherTrust.methods.executeWill(_testator, _id).send({from: ethereum.selectedAddress, to:contractAddress});
+    }
+  
+  const list=document.getElementById('list-button');
+    list.onclick = async ()=> {
+    checkConnected();   
+    
+    EtherTrust.methods.numTestators().call(async function(err, num){
+     if(!err){
+   DeleteRows();
+       
+     for (i=0;i<num;i++){//loops through all testators
+       var testator=await EtherTrust.methods.testators(i).call();
+       console.log(testator);
+       var creaDate=await EtherTrust.methods.lastAlive(testator).call();
+       console.log(creaDate);
+       
+       var wills=await EtherTrust.methods.getWills(testator).call();
+       console.log(wills);
+       var now = (await web3.eth.getBlock(await web3.eth.getBlockNumber())).timestamp;
+        for (var j=0;j<wills.length;j++){
+          console.log(now);
+          if (parseInt(now)>parseInt(creaDate)+parseInt(wills[j][2])){
+          //checks if will is mature
+          console.log(wills[j][1]);
+          console.log(wills[j][3]);
+          //to do: check that this j corresponds to the index of the testator's will in all cases (deletion, etc.)
+          addRows(wills[j][3], j, wills[j][1]);
+          }
+
+         }
+     }
+      document.getElementById('list-button').innerHTML="Refresh";
+     }
+      document.getElementById('willsDiv').style.display='block';
+
+
+ });
     }
 
 }
@@ -416,9 +627,12 @@ window.addEventListener('load', (event) => {
 if (typeof window.ethereum !== 'undefined'){
         console.log('Wallet detected');
         const ethereumButton = document.getElementById('wallet-button');
+        
         ethereumButton.addEventListener('click', () => {
+        
         //Will Start the metamask extension
         connectWallet();
+
         
 });
     
